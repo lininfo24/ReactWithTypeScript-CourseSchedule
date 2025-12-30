d86dc72e2540a16dcb2f1849608329d1c709075d
Doodle <23706830+cafedoodle@users.noreply.github.com>
2025-12-30 16:23:53 -0600

    Add Playwright tests

--- reactwithtypescript/tests/example.spec.ts
+++ reactwithtypescript/tests/example.spec.ts
@@ -1,18 +1,32 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/reactwithtypescript/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("user navigates to Info page from home", async ({ page }) => {
+  await page.goto("/"); // baseURL is set in config
+
+  // Click the "Info" link
+  await page.getByRole("link", { name: "Info" }).click();
+
+  // Assert that "Info Page" is now visible
+  await expect(page.getByText("Info Page")).toBeVisible();
+  // Assert that the heading is present
+  await expect(page.getByRole("heading", { name: "Info Page" })).toBeVisible();
+});
+
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole("link", { name: "Get started" }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "Installation" })
+  ).toBeVisible();
 });
